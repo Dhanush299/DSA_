9a2ec9ab88b4b4eaf7aa744085c15c4bf3971962
Dhanush299 <ds831518@gmail.com>
2026-07-19 22:13:15 +0530

    left rotate array by one

---
+++ Array/left_rotate_one.py
@@ -0,0 +1,10 @@
+def left_rotate_one(arr):
+    first=arr[0]
+    for i in range(len(arr)-1):
+        arr[i]=arr[i+1]
+        
+    arr[-1]=first
+    return arr
+    
+arr=[1,2,3,4,5]
+print(left_rotate_one(arr))
